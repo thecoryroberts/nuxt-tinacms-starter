e5ae81a9957432e474b42ee0853313f4ac0225fe
Cory Roberts <mail@coryroberts.net>
2024-08-10 23:29:55 +0000

    Initial Commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxt/content"],
-
   css: ["~/assets/main.css"],
+  compatibilityDate: "2024-08-10",
 });
